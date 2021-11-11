e7f7d19e6c0d920297ecc0ec6d96e51d15be77a7
VinhTran <87302636+tranhuuvinh1109@users.noreply.github.com>
2021-11-12 00:56:46 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "MyString.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+   cout<<"1"<<endl;
+    myString x("ab");
+    myString y("vinhssss");
+    myString z ;
+    z=x+y;
+    
+    cout<<"result: "<<z<<endl;
+    return 0;
+}
